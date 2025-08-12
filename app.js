2d8f43191b8fa58c65c1057bd96d4a1d816869a1
Jhon Cáceres Sandoval <jwcaceres1@soy.sena.edu.co>
2025-08-12 18:27:50 -0500

    Juego_Amigo_Secreto_Alura_Versión1
    Se agregaron las funciones principales:
    - agregarAmigo()
    - actualizarListaAmigos()
    - sortearAmigo()
    - reiniciarJuego()
    
    Además, se mejoró la experiencia de usuario:
    - Se añadió el evento onkeydown en el input para permitir ingresar nombres con la tecla "Enter".
    - Se incorporó el botón "Reiniciar juego" para limpiar la lista y el estado actual.

--- app.js
+++ app.js
@@ -1,1 +1,81 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let numeroMaximo = 10;
+let listaNumerosSorteados = [];
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+
+
+function asignarTextoElemento(elemento, texto){
+let elementoHTML = document.querySelector(elemento);
+elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento() {
+    //Ingreso Datos Usuario
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    //Revisión datos
+    console.log(`Intento del usuario: ${numeroDeUsuario}, Número secreto: ${numeroSecreto}`);
+
+    if(numeroDeUsuario===numeroSecreto) {
+        //El usuario acertó
+        asignarTextoElemento('p',`Acertaste el número secreto en ${intentos} ${intentos === 1 ? 'intento' : 'intentos'}`)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else{
+        //El usuario no acertó
+        if(numeroDeUsuario >   numeroSecreto){
+            asignarTextoElemento('p','El número Secreto es menor')
+        } else {
+            asignarTextoElemento('p','El número Secreto es mayor')
+        }
+        //Incremeta número intentos
+        intentos++;
+        //Limpia la caja
+        limpiarCaja();
+    }
+    return;
+            
+}   
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value='';
+    }
+//Generación número Secreto
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+        
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Validar si ya sorteamos todos los numeros
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles')
+    } else{
+        //Si el numero está en la lista, hacemos otra operación
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento ('h1', 'Juego del Número Secreto')
+    asignarTextoElemento ('p', `Indica un número del 1 al ${numeroMaximo}`)
+    numeroSecreto= generarNumeroSecreto();
+    intentos=1;
+    console.log(numeroSecreto);
+       
+}
+
+function reiniciarJuego(){
+    //Limpiar caja
+    limpiarCaja();
+    //Resstablecer condiciones iniciales
+    condicionesIniciales();
+    //Deshabilitar botón nuevo juegos
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+condicionesIniciales();
